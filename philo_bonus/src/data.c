7d3fce44c55a41720efd53cc8bb05b09c68def54
SenseiSamy <samy.naji2003@gmail.com>
2023-06-21 16:08:28 +0200

    think_time = 1 et meilleur parsing

--- philo_bonus/src/data.c
+++ philo_bonus/src/data.c
@@ -6,7 +6,7 @@
 /*   By: snaji <snaji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 21:29:56 by snaji             #+#    #+#             */
-/*   Updated: 2023/06/13 16:48:28 by snaji            ###   ########.fr       */
+/*   Updated: 2023/06/21 16:07:56 by snaji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,25 @@ static t_philo	*create_philos(int n)
 	return (philos);
 }
 
-static int	check_values(t_data *data)
+static int	get_value(char *value)
+{
+	int	i;
+
+	i = 0;
+	while (value[i])
+	{
+		if (value[i] < '0' || value[i] > '9')
+			return (-1);
+		++i;
+	}
+	return (ft_atoi(value));
+}
+
+static int	check_values(t_data *data, int ac)
 {
 	if (data->number_of_philosophers <= 0 || data->time_to_die < 0
-		|| data->time_to_eat < 0 || data->time_to_sleep < 0)
+		|| data->time_to_eat < 0 || data->time_to_sleep < 0
+		|| (ac == 6 && data->number_of_times_each_philosopher_must_eat < 0))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
@@ -52,17 +67,16 @@ int	init_data(int ac, char **av, t_data *data)
 		return (write(2, "philo: not enough argument\n", 27), EXIT_FAILURE);
 	else if (ac > 6)
 		return (write(2, "philo: too many arguments\n", 26), EXIT_FAILURE);
-	data->number_of_philosophers = ft_atoi(av[1]);
-	data->time_to_die = ft_atoi(av[2]);
-	data->time_to_eat = ft_atoi(av[3]);
-	data->time_to_sleep = ft_atoi(av[4]);
-	if (check_values(data) == EXIT_FAILURE)
-		return (write(2, "philo: invalid argument\n", 24), EXIT_FAILURE);
+	data->number_of_philosophers = get_value(av[1]);
+	data->time_to_die = get_value(av[2]);
+	data->time_to_eat = get_value(av[3]);
+	data->time_to_sleep = get_value(av[4]);
 	data->number_of_times_each_philosopher_must_eat = -1;
 	if (ac == 6)
-		data->number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
-	data->think_time = (data->time_to_die - (data->time_to_eat
-				+ data->time_to_sleep)) / 2;
+		data->number_of_times_each_philosopher_must_eat = get_value(av[5]);
+	if (check_values(data, ac) == EXIT_FAILURE)
+		return (write(2, "philo: invalid argument\n", 24), EXIT_FAILURE);
+	data->think_time = 1;
 	if (gettimeofday(&data->init_time, NULL) == -1)
 		return (write(2, "philo: initialization failed\n", 29), EXIT_FAILURE);
 	data->philos = create_philos(data->number_of_philosophers);
